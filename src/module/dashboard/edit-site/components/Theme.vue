decc453efb25f33e2e4b6f1b65288a4de4c532a8
Mikyas Alemayehu <mikyas.alemayehu@knands.com>
2019-11-24 23:07:23 +0300

    working on edit website

---
+++ src/module/dashboard/edit-site/components/Theme.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <v-card flat class="text-xs-center ma-3 c-card"
+            :class="{'c-active': theme.active}"
+        >
+             <v-card-text>
+                <v-img
+                    :src="require('@/assets/mint.png')"
+                    crossorigin="anonymous"
+                    :lazy-src="require('@/assets/loading.png')"
+                    aspect-ratio="1.7"
+                    class="theme-img"
+                    position=""
+                    contain
+                >
+                </v-img> 
+                <div class="mt-3">Theme - {{theme.name}}</div>
+                <p class="grey--text">{{theme.description}}</p>
+                <v-btn
+                    v-if="!theme.active"
+                    round
+                    color="c_selected_btn white--text text-capitalize c-btn my-2" 
+                >
+                    <v-icon small class="mr-2">brush</v-icon>
+                    <span class="mr-2">Use this theme</span>
+                </v-btn>
+                <v-btn
+                    v-if="theme.active"
+                    disabled
+                    round
+                    color="c_selected_btn white--text text-capitalize c-btn my-2" 
+                >
+                    <v-icon small class="mr-2">brush</v-icon>
+                    <span class="mr-2">Using This Theme</span>
+                </v-btn>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: Object,
+      required: true
+    }
+  },   
+}
+</script>
+
+<style scoped>
+.c-btn {
+    min-width:   130px;
+}
+.theme-img {
+    width: 100%;
+}
+.c-active {
+    border: .1em solid #fc4b6c;
+    border-radius: 4px;
+}
+</style>
--- src/module/dashboard/edit-site/views/EditSite.vue
+++ src/module/dashboard/edit-site/views/EditSite.vue
@@ -3,9 +3,16 @@
     <v-container class="c-body pa-0" fluid="true">
       <Navbar parent="editSite" icon="brush"/>
 
-      <v-card class="mx-4 my-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic ullam ipsam sapiente eveniet nisi dolorum dicta provident! Perferendis alias voluptatem veritatis nisi libero deserunt est nemo quia odio qui.  
-      </v-card>
+      <v-layout row wrap
+        class="my-4"
+        v-bind:class="$vuetify.breakpoint.xsOnly && 'mx-1' || 'mx-4'"
+      >
+        <v-flex xs12 sm6  v-for="theme in themes" v-bind:key="theme.name">
+          <Theme :theme="theme"/>
+        </v-flex>
+      </v-layout>
+
+      <div style="height: 51px"></div>
       
     </v-container>
 
@@ -14,11 +21,18 @@
 <script>
 
 import Navbar from '@/components/BodyNav'
+import Theme from '../components/Theme'
 
 export default {
   name: 'editSite',
   components: {
-    Navbar
+    Navbar, Theme
+  },
+  data() {
+    return {
+      themes: [{name: 'Mint', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam enim ab fugit illum sed exercitationem, quos quo. Delectus beatae exercitationem quibusdam doloribus aliquid alias ad omnis. Magnam doloremque perferendis error.', active: false},
+               {name: 'Mint2', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam enim ab fugit illum sed exercitationem, quos quo. Delectus beatae exercitationem quibusdam doloribus aliquid alias ad omnis. Magnam doloremque perferendis error?', active: true}]
+    }
   },
   created() {
       this.$store.commit('dashboard/SET_ACTIVE_PAGE', 'editSite');
@@ -26,8 +40,6 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-  .e4
-    width: 400px
-    margin: auto
+<style scoped>
+
 </style>
